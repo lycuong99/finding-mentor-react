4f6a4a84e1cdd8305acb1d8f454ceca958edc03a
lycuong99 <lycuong99@gmail.com>
2021-11-25 20:34:25 +0700

    change link

--- src/apis/fm.js
+++ src/apis/fm.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const testAPI = 'https://localhost:44307/api/';
 const official = "http://beclean.store:2020/api/";
 export default axios.create({
-    baseURL:testAPI
+    baseURL:official
 });
